79942a3446f119c0f90c1dd7b31c23f01fa956f4
Badhon <prashomanchakrabarti1@gmail.com>
2023-05-17 23:44:50 +0600

    google login Done

--- src/Pages/Login/Social/Social.jsx
+++ src/Pages/Login/Social/Social.jsx
@@ -1,8 +1,23 @@
+import { useContext } from "react";
 import { FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../../../provider/AuthProvider";
+
 const Social = () => {
+  const { logInGoogle } = useContext(AuthContext);
+
+  const handleGoogle = () => {
+    logInGoogle()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
-      <button className=" text-white btn btn-warning">
+      <button onClick={handleGoogle} className=" text-white btn btn-warning">
         <FaGoogle className="me-4 text-cyan-600"></FaGoogle> Google
       </button>
     </div>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,19 +1,54 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
-const Auth = getAuth(app);
-const AuthProvider = ({ Children }) => {
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const AuthProvider = ({ children }) => {
   const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(false);
 
   //created user
+  const createdUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  //login email and password
+  const logInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
+  //loginUser
+
+  //google with login
+  const logInGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //logOut
+  const loggedOut = () => {
+    return signOut(auth);
+  };
   const allInfo = {
     user,
+    createdUser,
+    logInUser,
+    logInGoogle,
+    loading,
+    loggedOut,
   };
+
+  //console.log(allInfo);
   return (
-    <AuthContext.Provider value={allInfo}>{Children}</AuthContext.Provider>
+    <AuthContext.Provider value={allInfo}>{children}</AuthContext.Provider>
   );
 };
 
